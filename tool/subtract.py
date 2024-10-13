b9b2ee79dd0e2997bac7a61526a5df50c39e6614
Jingyi Song <80659762+JingJing1016@users.noreply.github.com>
2024-10-13 19:49:38 +0800

    tool: use `print` rather than `rich.console`

--- tool/subtract.py
+++ tool/subtract.py
@@ -1,11 +1,5 @@
 import re
-from rich.traceback import install
-
-install(extra_lines=2)
-
-from rich.console import Console
-from rich.table import Table
-from rich.box import SIMPLE
+from datetime import datetime
 
 # HACK: 荒莳队员名单
 our_players = [
@@ -28,25 +22,17 @@ our_players = [
 ]
 
 
-def substract(player_scores: str) -> None:
+def subtract(player_scores: str) -> None:
     """
     计算并打印其他人和荒莳的点差
 
     Args:
         player_scores (str): 包含四家分数的指定格式的字符串，建议直接从联赛群复制机器人消息
-
-    Raises:
-        ValueError: 输入玩家数量不为4
-        ValueError: 输入不包含荒莳队员雀魂id
     """
-
-    console = Console()
 
     # 提取四家名字和分数
     pattern = re.compile(r"([^\n\r ,]+) (\d+)")
     match = pattern.findall(player_scores)
-    if len(match) != 4:
-        raise ValueError("输入玩家数量不为4")
 
     # 检测队员 index
     index = -1
@@ -54,34 +40,49 @@ def substract(player_scores: str) -> None:
         if match[i][0] in our_players:
             index = i
             break
-    if index == -1:
-        raise ValueError("输入不包含荒莳队员雀魂id")
-
-    # 设置表格
-    table = Table(
-        title=f"\n{match[index][0]}", title_style="bold", box=SIMPLE, show_header=False
-    )
 
     # 计算点差并打印
+    print("\n")
+    fmt_str = "{0:<10}{1}"
     for i in range(4):
         if i == index:
             continue
         diff = int(match[index][1]) - int(match[i][1])
         if diff <= 0:
-            sign = "[indian_red1]+"
+            sign = "+"
             diff = -diff
+            color_code = "\033[0;31m"  # 红色
         else:
-            sign = "[green]-"
-        table.add_row(match[i][0], f"{sign}{str(diff)}")
-    console.print(table)
+            sign = "-"
+            color_code = "\033[0;32m"  # 绿色
+        print(
+            fmt_str.format(match[i][0], f"{color_code}{sign}{diff}\033[0m")
+        )
+    print("\n")
 
 
 if __name__ == "__main__":
-    user_input = input(
+    print(
         """
-包含如“谜拟Q侠 123500,悦颖嘉熙 105000,济士白 113800,破败军团 57700”的字符串
-以英文逗号分隔玩家，以空格分隔每个玩家的昵称和分数
+===============================================================
 
-请输入四家分数: """
+    示例输入：
+    谜拟Q侠 123500,悦颖嘉熙 105000,济士白 113800,破败军团 57700
+    
+    提示：
+    - 建议直接复制机器人消息
+    - 必须包含荒莳队员的雀魂名称
+
+===============================================================
+
+"""
     )
-    substract(user_input)
+    while True:
+        current_time = datetime.now().strftime("%H:%M:%S")
+        # user_input = console.input(f"[{current_time}] :glowing_star: 请输入：")
+        user_input = input(f"[{current_time}] 请输入：")
+        try:
+            subtract(user_input)
+        except Exception as e:
+            # console.print(f":hankey: 输入格式错误\n")
+            print(f"输入格式错误\n")
